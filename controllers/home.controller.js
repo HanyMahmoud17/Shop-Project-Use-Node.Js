b542ae2dd8079f8098e56ea1e07e8420ca2f6567
hamymah <hanymah823@gmail.com>
2023-07-17 20:55:57 +0200

    make filter by category

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -4,13 +4,50 @@ const productModel=require('../models/products.model')
 
 
 // here i make the middle
-exports.getHome= (req,res,next)=>{
-    // first i need to get product using the function that was in the model
-    productModel.getAllProducts().then(products => {
-        // noe i render the product in view page
-        res.render('index',{
-            // take with you
-            products: products
-        })
-    })
-}
+// exports.getHome= (req,res,next)=>{
+//     // first i need to get product using the function that was in the model
+//     productModel.getAllProducts().then(products => {
+//         // noe i render the product in view page
+//         res.render('index',{
+//             // take with you
+//             products: products
+//         })
+//     })
+
+    
+//     let category = req.query.category;
+//     if (category && category !== 'all') {
+//       productModel.getProductByCategory(String(category)).then(products => {
+//         res.render('index', {
+//           products: products
+//         });
+//       });
+//     } else {
+//       productModel.getAllProducts().then(products => {
+//         res.render('index', {
+//           products: products
+//         });
+//       });
+//     }
+
+
+// }
+
+exports.getHome = (req, res, next) => {
+    let category = req.query.category;
+    let promise;
+    if (category && category != 'all') {
+      promise = productModel.getProductByCategory(category);
+    } else {
+      promise = productModel.getAllProducts();
+    }
+  
+    promise.then(products => {
+      res.render('index', {
+        products: products
+      });
+    }).catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+  };
--- models/products.model.js
+++ models/products.model.js
@@ -1,36 +1,32 @@
-const mongoose=require('mongoose');
-// this is the db
-const DB_URL='mongodb://127.0.0.1:27017/online-shop';
+const mongoose = require('mongoose');
+const db = require('../Config/connectDB');
 
+const productSchema = mongoose.Schema({
+  name: String,
+  price: Number,
+  category: String,
+  description: String,
+  image: String
+});
 
+const Product = mongoose.model('product', productSchema);
 
-const productSchema=mongoose.Schema({
-    name:String,
-    price:Number,
-    category:String,
-    description:String,
-    image:String
-})
+exports.getAllProducts = () => {
+  return new Promise((resolve, reject) => {
+    Product.find({})
+      .then(products => {
+        resolve(products);
+      })
+      .catch(err => reject(err));
+  });
+};
 
-const Product=mongoose.model('product',productSchema);
-
-exports.getAllProducts=()=>{
-
-    // now i make my promisse to pass it to the controller
-
-    return new Promise((resolve,reject)=>{
-        //conect db + i make promise chain
-        mongoose.connect(DB_URL).then(()=>{
-            // get all products
-            console.log("server connected");
-            return Product.find({})
-        }).then(products=>{
-            mongoose.disconnect()
-            // console.log("server disconnected");
-            // here after i get all products i resolve it
-            resolve(products)
-        }).catch(err=> reject(err))
-    })
-
-
-}
+exports.getProductByCategory = category => {
+  return new Promise((resolve, reject) => {
+    Product.find({ category: category })
+      .then(products => {
+        resolve(products);
+      })
+      .catch(err => reject(err));
+  });
+};
