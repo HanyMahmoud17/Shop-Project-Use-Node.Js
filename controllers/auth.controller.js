138966c20c9df0b8c2bc075383b4f287b59826af
hamymah <hanymah823@gmail.com>
2023-07-19 02:11:56 +0200

    make validator to signup form using express-validator

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,25 @@
 const authModel=require('../models/auth.model')
+const validationResult=require('express-validator').validationResult
 
 exports.getSignup=(req,res,next)=>{
-        res.render('signup')
+    // console.log(req.flash('authError')[0]),
+        res.render('signup',{
+            authError:req.flash('authError')[0],
+            validationErrors: req.flash('validationErrors')
+        })
 }
 
 exports.postSignup=(req,res,next)=>{
+    if(validationResult(req).isEmpty()) {
     authModel.createNewUser(req.body.username,req.body.email,req.body.password).then(()=>res.redirect('/login')).catch(err=>
         {
-            console.log(err);
+            req.flash('authError',err)
             res.redirect('/signup')
         });
+    } else {
+        req.flash('validationErrors',validationResult(req).array());
+        res.redirect('/signup');
+    }
 }
 
 exports.getLogin=(req,res,next)=>{
